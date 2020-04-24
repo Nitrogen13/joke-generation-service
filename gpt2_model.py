f9045f08efa1d6d53fa4116c9560648e2f8b54b6
Nitrogen <ilgizar1313@gmail.com>
2020-04-24 13:48:59 +0300

    refactor: small refactoring

--- gpt2_model.py
+++ gpt2_model.py
@@ -7,7 +7,18 @@ from ruGPT2.arguments import get_args
 from ruGPT2.generate_samples import prepare_tokenizer, setup_model
 from ruGPT2.pretrain_gpt2 import initialize_distributed
 
-from constants import MPSIZE, NLAYERS, NHIDDEN, CHECKPOINT_PATH, NATT, TOKENIZER_PATH, MAXSEQLEN, TEMP, TOPK, TOPP
+from constants import (
+    MPSIZE,
+    NLAYERS,
+    NHIDDEN,
+    CHECKPOINT_PATH,
+    NATT,
+    TOKENIZER_PATH,
+    MAXSEQLEN,
+    TEMP,
+    TOPK,
+    TOPP, MAX_POSITIONAL_EMBEDDING,
+)
 
 torch.backends.cudnn.enabled = True
 
@@ -19,7 +30,7 @@ ARGS.num_layers = NLAYERS
 ARGS.hidden_size = NHIDDEN
 ARGS.load = str(CHECKPOINT_PATH)
 ARGS.num_attention_heads = NATT
-ARGS.max_position_embeddings = 1024
+ARGS.max_position_embeddings = MAX_POSITIONAL_EMBEDDING
 ARGS.tokenizer_type = "RubertaBPETokenizer"
 ARGS.tokenizer_path = str(TOKENIZER_PATH)
 ARGS.fp16 = True
--- tests/test_travis.py
+++ tests/test_travis.py
@@ -1,7 +1,4 @@
-import unittest
-
 import logging
-import unittest
 from pathlib import Path
 import pytest
 import pook
@@ -48,7 +45,7 @@ def get_mock_message(text):
 	
     
 @pytest.yield_fixture(name='bot')
-async def bot_fixture():
+async def bot_fixture(event_loop):
     """ Bot fixture """
     pook.on()
     _bot = Bot(API_TOKEN)
